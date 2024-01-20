e0085e566dfac26df6998c64c46493caf5a421f5
Muhammad Alfaiz Khalifah Alamsyah <faiz_doc@outlook.com>
2024-01-20 09:28:42 +0700

    Set up awal

---
+++ main.mjs
@@ -0,0 +1,20 @@
+import fs from "fs";
+import process from "process";
+import readlinePromises from "readline/promises";
+
+
+let input = readlinePromises.createInterface({
+    input:process.stdin,
+    output:process.stdout
+})
+
+const opsi = ["CREATE", "READ", "UPDATE", "DELETE", "LIST"];
+const files = [];
+
+//Fungsi create
+
+//Fungsi Read
+
+//Fungsi update
+
+//Fungsi Delete
