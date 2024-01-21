32e3888cbebab70f6c7a1d4db473129e5686c97a
Muhammad Alfaiz Khalifah Alamsyah <faiz_doc@outlook.com>
2024-01-21 22:00:41 +0700

    Memperbaiki masalah asinkronus

--- main.mjs
+++ main.mjs
@@ -1,4 +1,4 @@
-import fs from "fs";
+import { promises as fsPromises } from 'fs';
 import process from "process";
 import readlinePromises from "readline/promises";
 
@@ -8,88 +8,116 @@ let input = readlinePromises.createInterface({
     output:process.stdout
 })
 
-const opsi = ["CREATE", "READ", "UPDATE", "DELETE", "LIST"];
-
-
-
-
-
+const opsi = ["CREATE", "READ", "CHANGE", "UPDATE", "DELETE", "LIST"];
 const files = [];
 let status = true;
-    while(status){
-        
+
+    async function main(){
+        while(status){
             console.log("Masukkan perintah yang kamu mau");
             list(opsi);
             let perintah = await input.question("Masukkan perintah : ");
            
             if(perintah.toLocaleLowerCase()==opsi[0].toLocaleLowerCase()){
-                let masukan = await input.question("Masukkan nama file yang ingin kamu buat : ");
-                let namaFile = masukan.concat(".txt");
-                fs.writeFile(namaFile,"",(err,data)=>{
-                    if(err){
-                        console.log("terjadi error saat membuat file");
-                    }
-                    console.log(data);
-                });
-                
-            }else if(perintah.toLocaleLowerCase()==opsi[1].toLocaleLowerCase()){
-                let masukan = await input.question("Masukkan nama file yang ingin kamu baca : ");
-                let namaFile = masukan.concat(".txt");
-                fs.readFile(namaFile,(err, data)=>{
-                    if(err){
-                        console.log("Error, tidak bisa membaca file");
-                    }
-                    console.log(data.toString());
-                   
-                })
+                let masukan = await input.question("Masukkan nama file yang ingin dibuat : ");
+                const respon = await createFile(masukan);
+                files.push(masukan); 
+                console.log(respon);
+            }
+            else if(perintah.toLocaleLowerCase()==opsi[1].toLocaleLowerCase()){
+               let masukan = await input.question("Masukkan nama file yang ingin dibaca : ");
+               const data = await readFile(masukan);
+               console.log(data);
             }else if(perintah.toLocaleLowerCase()==opsi[2].toLocaleLowerCase()){
-                let nama = await input.question("Masukkan nama file yang ingin kamu update : ");
-                let tulisan = await input.question("Masukkan tulisanmu : \n");
-                let namaFile=nama.concat(".txt");
-                fs.writeFile(namaFile,tulisan,(err, data)=>{
-                    if(err){
-                        console.log("Update file error")
-                    }
-                    console.log(data)
-                })
-            }else if(perintah.toLocaleLowerCase()==opsi[3].toLocaleLowerCase()){
-                let masukan = await input.question("Masukkan nama file yang ingin kamu hapus : "); 
-                let namaFile=masukan.concat(".txt");
-                fs.unlink(namaFile,(err)=>{
-                    if(err){
-                        console.log("Error saat menghapus file");
-                    }
-                    console.log("Data berhasil dihapus");
-                    
-                })
-            }else{
+                let nama = await input.question("Masukkan nama file yang ingin diubah : ");
+                let tulisan = await input.question("Masukkan tulisan yang ingin dimasukkan ke file : \n");
+                const respon = await changeFile(nama,tulisan);
+                console.log(respon);
+            }else if(perintah.toLocaleLowerCase()==opsi[3].toLowerCase()){
+                let nama = await input.question("Masukkan nama file yang ingin diupdate : ");
+                let tulisan = await input.question("Masukkan tulisan yang ingin dimasukkan ke file : \n");
+                const respon = await updateFile(nama,tulisan);
+                console.log(respon);
+            }
+            else if(perintah.toLocaleLowerCase()==opsi[4].toLocaleLowerCase()){
+                let masukan = await input.question("Masukkan nama file yang ingin dihapus : ");
+                const respon = await deleteFile(masukan);
+                let index = files.indexOf(masukan);
+                if(index!=-1){
+                    files.splice(index,1);
+                    console.log(respon);
+                }else{
+                    console.log("Elemen tersebut tidak ada di array");
+                }
+                
+            }else if(perintah.toLowerCase()==opsi[5].toLowerCase()){
+                if(files[0]=""){
+                    console.log("Tidak ada file lain dalam folder");
+                }else{
+                    list(files);
+                }
+            }
+            else{
                 console.log("Perintah tidak dikenal, keluar dari program");
                 process.exit(0);
             }
        
     }
+    }
+
+    
     
 
 //Fungsi create
-function createFile(nama ){
-    
+async function createFile(nama){
+    let namaFile = nama.concat(".txt");
+    await fsPromises.writeFile(namaFile,"",(err)=>{
+        if(err){
+            console.log("Error ketika membuat file");
+        }
+        
+    })
    
-   
+    return "File berhasil dibuat";
 }
 
 //Fungsi Read
-function readFile(nama){
-    
+async function readFile(nama){
+    let namaFile = nama.concat(".txt");
+    const data = await fsPromises.readFile(namaFile)
+    return data.toString();
+}
+
+//Fungsi change
+async function changeFile(nama, tulisan){
+    let namaFile = nama.concat(".txt");
+    await fsPromises.writeFile(namaFile,tulisan,(err)=>{
+        if(err){
+            console.log("Error saat mengubah file");
+        } 
+    })
+    return "File berhasil di-update";
 }
 
 //Fungsi update
-function writeFile(nama, tulisan){
-    
+async function updateFile(nama, tulisan){
+    let namaFile = nama.concat(".txt");
+    await fsPromises.appendFile(namaFile,tulisan,(err)=>{
+        if(err){
+            console.log("Error saat meng-update file")
+        }
+    })
+    return "File berhasil diupdate"
 }
-
 //Fungsi Delete
-function deleteFile(nama){
-    
+async function deleteFile(nama){
+    let namaFile = nama.concat(".txt");
+    await fsPromises.unlink(namaFile,(err)=>{
+        if(err){
+            console.log("Error saat menghapus file");
+        }
+    })
+    return "File berhasil dihapus";
 }
 //Fungsi List
 function list(array){
@@ -97,3 +125,5 @@ function list(array){
         console.log(array[i]);
     }
 }
+
+main();
--- testing.mjs
+++
@@ -1,8 +0,0 @@
-import fs from "fs";
-import process from "process";
-
-fs.writeFile("teks1","",(err)=>{
-    if(err){
-        console.log("terjadi error saat membuat file");
-    }
-});
