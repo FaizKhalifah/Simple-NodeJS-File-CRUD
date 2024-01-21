ec23452408fbc8bef5502e69727f92b20491ad67
Muhammad Alfaiz Khalifah Alamsyah <faiz_doc@outlook.com>
2024-01-21 08:17:01 +0700

    Menambah fungsi read, dan update

--- main.mjs
+++ main.mjs
@@ -11,26 +11,46 @@ let input = readlinePromises.createInterface({
 const opsi = ["CREATE", "READ", "UPDATE", "DELETE", "LIST"];
 const files = [];
 
+console.log("Masukkan perintah yang kamu mau");
+list(opsi);
+let perintah = await input.question("");
 
+createFile("teks1");
+list(files);
 
-createFile("teks4");
 
 //Fungsi create
 function createFile(nama){
     let namaFile = nama.concat(".txt");
-    fs.open(namaFile,'w',function(err,file){
-        if(err){
-            throw err;
-        }else{
-            console.log(`File dengan nama ${namaFile} berhasil ditambah`);
-        }
-    })
-    let deskriptorFile = fs.openSync(namaFile);
-    fs.close();
+    fs.writeFile(namaFile,"",()=>{
+        console.log("File telah dibuat");
+    });
+    files.push(nama.toString());
 }
 
 //Fungsi Read
-
+function readFile(nama){
+    let namaFile = nama.concat(".txt");
+    fs.readFile(namaFile,(err, data)=>{
+        if(err){
+            console.log("Error, tidak bisa membaca file");
+        }else{
+            console.log(data.toString());
+        }
+    })
+}
 //Fungsi update
-
+function writeFile(nama, tulisan){
+    let namaFile=nama.concat(".txt");
+    fs.writeFile(namaFile,tulisan,()=>{
+        console.log("File berhasil di-update")
+    })
+}
 //Fungsi Delete
+
+//Fungsi List
+function list(array){
+    for(let i in array){
+        console.log(array[i]);
+    }
+}
