1161735323985a91d2f926f040b413af1de183ed
Muhammad Alfaiz Khalifah Alamsyah <faiz_doc@outlook.com>
2024-01-21 08:46:56 +0700

    Percobaan pertama menerima input dan error pada asinkronus

--- main.mjs
+++ main.mjs
@@ -10,23 +10,52 @@ let input = readlinePromises.createInterface({
 
 const opsi = ["CREATE", "READ", "UPDATE", "DELETE", "LIST"];
 const files = [];
+let status = true;
 
-console.log("Masukkan perintah yang kamu mau");
-list(opsi);
-let perintah = await input.question("");
 
-createFile("teks1");
-list(files);
+
+
+while(status){
+    console.log("Masukkan perintah yang kamu mau");
+    list(opsi);
+    let perintah = await input.question("");
+        if(perintah.toLocaleLowerCase()==opsi[0].toLocaleLowerCase()){
+            let masukan = await input.question("Masukkan nama file yang ingin kamu buat : ");
+            createFile(masukan.toString());
+        }else if(perintah.toLocaleLowerCase()==opsi[1].toLocaleLowerCase()){
+            let masukan = await input.question("Masukkan nama file yang ingin kamu baca : ");
+            readFile(masukan.toString());
+        }else if(perintah.toLocaleLowerCase()==opsi[2].toLocaleLowerCase()){
+            let nama = await input.question("Masukkan nama file yang ingin kamu update : ");
+            let tulisan = await input.question("Masukkan tulisanmu : \n");
+            writeFile(nama,tulisan);
+        }else if(perintah.toLocaleLowerCase()==opsi[3].toLocaleLowerCase()){
+            let masukan = await input.question("Masukkan nama file yang ingin kamu hapus : "); 
+            deleteFile(masukan);
+        }else{
+            console.log("Perintah tidak dikenal, keluar dari program");
+            process.exit(0);
+        }
+}
+
+
+
+
 
 
 //Fungsi create
 function createFile(nama){
     let namaFile = nama.concat(".txt");
-    fs.writeFile(namaFile,"",()=>{
-        console.log("File telah dibuat");
+    fs.writeFile(namaFile,"",(err)=>{
+        if(err){
+            console.log("terjadi error saat membuat file");
+        }else{
+            console.log("File telah dibuat"); 
+        }
     });
-    files.push(nama.toString());
+   
 }
+
 
 //Fungsi Read
 function readFile(nama){
@@ -34,9 +63,8 @@ function readFile(nama){
     fs.readFile(namaFile,(err, data)=>{
         if(err){
             console.log("Error, tidak bisa membaca file");
-        }else{
-            console.log(data.toString());
         }
+        console.log(data.toString());
     })
 }
 //Fungsi update
@@ -47,7 +75,15 @@ function writeFile(nama, tulisan){
     })
 }
 //Fungsi Delete
-
+function deleteFile(nama){
+    let namaFile=nama.concat(".txt");
+    fs.unlink(namaFile,(err)=>{
+        if(err){
+            console.log("Error saat menghapus file");
+        }
+        console.log("File berhasil dihapus");
+    })
+}
 //Fungsi List
 function list(array){
     for(let i in array){
